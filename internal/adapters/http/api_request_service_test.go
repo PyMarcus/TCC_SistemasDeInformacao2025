0e811bb910c24c6fd8f9f2f819a73522ec66c201
PyMarcus <marcus-v@outlook.com>
2025-04-22 14:08:37 -0300

    test(api_request_service): ensure that will done

--- internal/adapters/http/api_request_service_test.go
+++ internal/adapters/http/api_request_service_test.go
@@ -11,3 +11,26 @@ func TestNewApiRequestService(t *testing.T){
 
 	assert.NotNil(t, clientService)
 }
+
+func TestSetters(t *testing.T){
+	clientService := NewApiRequestService()
+
+	result := clientService.SetMethod("GET")
+
+	assert.NotEmpty(t, result)
+
+	result = clientService.SetURL("https://www.google.com")
+
+	assert.NotEmpty(t, result) 
+
+	result = clientService.SetBody("")
+
+	assert.NotEmpty(t, result) 
+
+	header := make(map[string]string)
+	header["hello"] = "world"
+	result = clientService.SetHeaders(header)
+
+	assert.NotEmpty(t, result) 
+	
+}
