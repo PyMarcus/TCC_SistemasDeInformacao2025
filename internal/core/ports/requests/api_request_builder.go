a20ac45c3aa1db69630c11e13b76a85e44584fbf
PyMarcus <marcus-v@outlook.com>
2025-04-22 14:37:03 -0300

    fix(api_request_builder): Fix to return response in build instead of request

--- internal/core/ports/requests/api_request_builder.go
+++ internal/core/ports/requests/api_request_builder.go
@@ -12,5 +12,5 @@ type APIRequestBuilder interface{
 	SetHeaders(headers map[string]string) APIRequestBuilder
 	SetBody(body string) APIRequestBuilder
 	SetTimeout(timeout time.Duration) APIRequestBuilder
-	Build() (*http.Request, error)
+	Build() (*http.Response, error)
 }
