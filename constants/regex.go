282682d926181c5848f1461341e67027aafce137
PyMarcus <marcus-v@outlook.com>
2025-04-24 15:47:14 -0300

    feat(constants): Added constants to clean class

---
+++ constants/regex.go
@@ -0,0 +1,8 @@
+package constants
+
+const (
+	SINGLE_COMMENTS string = `(?m)//[^\n]*\n`
+	MULTI_COMMENTS string = `(?s)/\*.*?\*/`
+	IMPORTS string = `(?m)^import\s+[^\n]+;\s*`
+	HEADERS string = `(?m)^package\s+[^\n]+;\s*`
+)
