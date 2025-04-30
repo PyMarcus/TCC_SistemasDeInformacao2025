be5547e36fa016394ddf6d5a470db4b2fe8f3be4
PyMarcus <marcus-v@outlook.com>
2025-04-30 15:42:13 -0300

    fix: change base code to use for one I.A agent

--- internal/core/domain/atom_model.go
+++ internal/core/domain/atom_model.go
@@ -6,15 +6,12 @@ type Atom struct {
 	ID                   uint   `gorm:"primaryKey"`
 	QuestionID           int    `gorm:"column:question_id"`
 	Question             string `gorm:"type:text"`
-	AgentOneAnswer       string `gorm:"type:text"`
-	AgentTwoAnswer       string `gorm:"type:text"`
+	Answer       		 string `gorm:"type:text"`
 	DatasetID            int    `gorm:"column:dataset_id"`
 	AtomSearched         string `gorm:"column:atom_searched"`
-	AtomFindedByAgentOne string `gorm:"column:atom_finded_by_agent_one"`
-	AtomFindedByAgentTwo string `gorm:"column:atom_finded_by_agent_two"`
-	AgentOneIsCorrect    bool   `gorm:"column:agent_one_is_correct"`
-	AgentTwoIsCorrect    bool   `gorm:"column:agent_two_is_correct"`
-	Failed               bool
+	AtomFinded			 string `gorm:"column:atom_finded"`
+	IsCorrect    		 bool   `gorm:"column:is_correct"`
+	Failed               bool   `gorm:"column:failed"`
 	ErrorID              int       `gorm:"column:error_id"`
 	CreatedAt            time.Time `gorm:"column:created_at;default:CURRENT_TIMESTAMP"`
 	UpdatedAt            time.Time `gorm:"column:updated_at"`
